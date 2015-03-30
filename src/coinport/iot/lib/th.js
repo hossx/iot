7549cead08458f24d475ea022de13d1733bcd935
yangli <yangli@coinport.com>
2015-03-30 17:49:22 +0800

    add interface for updating public data

--- src/coinport/iot/lib/th.js
+++ src/coinport/iot/lib/th.js
@@ -3,16 +3,32 @@
  * Author: xiaolu@coinport.com (Xiaolu Wu)
  */
 
-var Events = require('events'),
-    Util   = require("util");
+var Events     = require('events'),
+    Util       = require("util"),
+    Telehash   = require("/home/jaice/work/locker/node_modules/telehash"),
+    EventStream = require('/usr/local/lib/node_modules/event-stream');
+    Stream = require('/usr/local/lib/node_modules/stream');
 
-var TH = module.exports.TH = function() {
+var TH = module.exports.TH = function(endpoint, cb) {
     Events.EventEmitter.call(this);
+    this.endpoint = endpoint;
+    this.router = {"hashname": "toniwerfld7glee4xgfahbzygkqjv623dmjpq5kbjyca6cadcnvq","paths": [{"type":"udp4","ip":"192.168.0.6","port": 42424 },{"type":"udp4","ip":"192.168.36.1","port": 42424},{"type": "udp4", "ip": "172.16.192.1", "port": 42424},{"type": "http","url": "http://192.168.0.6:50299"},{"type": "tcp4","ip": "192.168.0.6","port": 42424} ],"keys": { "1a": "amwunbizmlgvn66dzcqlgq73ezsz73w7ee","2a": "gcbacirqbudaskugjcdpodibaeaqkaadqiaq6abqqiaquaucaeaqbuub2gvd4zs2ucjly2nsmi7k4i3s3zcs4dnlm2izllzy6uxk6h7fva5eu6xhib2c4bgfqsezw4oxkumty23rcrunp4ni3admwti7jeu5sghgouwrozlf7fczmw7uvzpjnrrcuft4drcwp2lllqixmtqomg73ggqumojc2uzvmt6zreqb7w3bodte3zr5sf3q34ilj22trkjcs6uktghkykmxtqc7lvco3jo2gyp2gldsfoptuutbhb2t5ko6dfcosheftnpvuwcuxbbxdzt4fjgjku3mirhf4mawxun3i2m5g5ady2cefcrqzpodlo5egkp46n7ur4bbskdepa3g267naizudr544fbhj7gsfuomrykdeejwhppmkqh2hfz5so52adid4grpn34sxuyiyvem2oicamaqaai","3a": "rka5rqje7haesp4tjilxcmhglwwaduczuw5drfoxyhycmzt2mj6a"},"router": true};
+    var self = this;
+    Telehash.mesh({id:this.endpoint}, function(err, mesh){
+      self.mesh = mesh;
+      var linkR = self.mesh.link(self.router);
+      EventStream.readArray([0]).pipe(linkR.stream());
+      // linkR.stream().pipe("ping router");
+      self.emit('mesh_ready');
+    });
 };
 Util.inherits(TH, Events.EventEmitter);
 
 TH.EventType = {
-    NEW_MESSAGE : 'new_message'  // emitted data like this: {from: xxx, message: xxx}
+    NEW_MESSAGE : 'new_message',  // emitted data like this: {from: xxx, message: xxx}
+    RECEIVED_MESSAGE : 'received_message',
+    NEW_HASHNAME : 'new_hashname',
+    MESH_READY : 'mesh_ready'
 };
 
 /**
@@ -23,24 +39,31 @@ TH.EventType = {
  * @return {boolean} Success or Fail
  */
 TH.prototype.sendMessage = function(to, message) {
+  if (this.mesh != null) {
+    this.mesh.link(to, function(e, l){
+      EventStream.readArray(message).pipe(l.stream());
+      // l.stream().pipe(message);
+    });
+  } else {
+    console.log("mesh is not init");
+  }
 };
 
 TH.prototype.listen = function() {
+  var self = this;
+  this.mesh.accept = this.mesh.link;
+  this.mesh.stream(function(link, req, accept){
+    console.log("start receive message from p2p network")
+    accept().pipe(EventStream.writeArray(function(err, message){
+      console.log("message : ", message);
+      self.emit(TH.EventType.RECEIVED_MESSAGE, req, message);
+    }));
+  });
 };
 
-TH.generateEndpoint = function(callback) {
-    var endpoint = {
-        keys: {
-            '1a': 'akhvxp7jixc5njelrm3df6el3vlgg6kpr4',
-            '2a': 'gcbacirqbudaskugjcdpodibaeaqkaadqiaq6abqqiaquaucaeaqbivb6wbbatlztxqn2il6da5nft3fyt3lxrfz5d3nsuzdeaqc4v5nlhq32xt7bf4et2xxmtkw3hq6cpyotwbw4jpfwbt6u37hlgjxctshiruf3pr47fcxwfsr4425o4jugq3kh5m3sl3fjn6wdqog55yuvitf5qqmen3kqn4gz5324lldrlrbpsr5y7lsafpmedwq57e2j4qaa4cvzt2dkykntxqruqncm4p2n7zpasfjeb2vgtrrkhz26jgy6y2s2absmwnbz5o2zdnspotkz4xu7sc65bxgsiuoquuoeufo7pyjrpmk2um3vyq3uss3xrk22luiw6dxjneoms3t4o7pxy2vv4rvo23gb6ri6pemwqjxym7j2mmam4izgvde3oaqdpakhyjwrwalvirfpqbvxxycamaqaai',
-            '3a': '67e6ci4ufkmxdguvebfbe6jv6vsfrxlnkvzs6qhtud7ilhhlebrq'
-        },
-        secrets: {
-             '1a': 'uuiu4knnhd7vneev7pmbemuju5yqkuyd',
-             '2a': 'gcbajjacaeaafaqbaeakfipvqiie26m54dosc7qyhljm6zoe6254jopi63mvgizaeaxfplkz4g6v47yjpbe6v53e2vwz4hqt6du5qnxclznqm7vg7z2zsnyu4r2enbo34phziv5rmuphgxlxcnbug2r7lg4s6zklpvq4drxpoffkezpmedbdo2udpbwpo6xc2y4k4il4upoh24qbl3ba5uhpzgspeaahavom6q2wctm54enedithd6tp6lyerkjaovju4mkr6oxsjwhwguwqamtftioplwwi3mt3u2wpf5h4qxxinzusfduffdrfblx36cml3cwvdg5oeg5euw54kwws5cfxq52ljdtew47dx356gvnpenlwwzqpukhtzdfucn6dh2otdadhcgjvizg3qea3ycr6cnunqc5kejl4ann56aqdaeaacaucaeah3godo7oywwa7tqugxu3mo4izizzoqs3wgkmjjafxg35pihvuq73s5eve2lvdnoupvndryv5vcib5tjjz6tst5e3to7w5bwwfrvpxm2yh4pvheh5zbp3bdogtnx52gtad74tqk2spi65qt53l4jy3rfgxbotsocre4fqoz4kkrvgzc5cngbv5wh3xsutqzbz6v2lge47sepxn6gmh5kv24z3hfuddkewbs6mlicgbixzfsfblp4ojk7scbu2czashetwwv62kla5t42isufdc6x2p27rf37xdw44nyqi26khgzusfqgy3sovwco6qaglma25wrkk23teahmgu3m5quswbii5w5rwpaddhaho6bpnoz3lopfbtqb57qenkd23igvryue4kdzzprga6fa25zwnucaubqeannzi4jbkh5rjwn6bvobpyardb24dajyt4ccx5fclb4sh5rslbvunafghn44djpyhpwh3v6zjj7ggmybuwmoa4m5sm3r6nqrpmhmhbyfrg2cqxuipatlhf3m4d5zp3g6wzkyajgk3qy7lt25mzlzpr5cy3vwbzxwoi4xyf2nangk2mhzmpkn4ncrck3wjsh4wcsumyaktcwaqrakaycagbxwyfvnbciiqp7cksgulm56zykl2wmbfljrml2yuipm4jnxbadnfbmp2ovdpl4s5t5qh3iglusmfvp2kzgk3wypoi3i4gwaw6pykooqwasayxzxrmyalwak3incpojbpwhbnho7af7hfrpsia2b6vw5vig2zqtrac65ap2lhaao3s453qw4thajbs5rkt3bai7g43qlchr3xqfambadfvchn7hfrg7se7duhnv4g3njjtihmkcsdmvfgxf6qrxqpyt6kgipj22slzjf622ls3tudl5osga53ittid7stp6cu5thus3txb3du2345pot4a5uli4zjtki3aa5p5fhmkq4sln5ypxsh4heqf7xgpejepxbzv4td3oe6v3iaxc56drv5qjqqqferkvwvfozwos277ewsi6yicqgaqbb3hgzml4ktpim3ljv4crekvdgduzjjwtep2ylhgddpxnb7g6c7vm2nrhvn2ziu76kezeqciaapmh3zewahpequh6ko76gwebdqvliykfa4iqm63zxkty5biym7fmkpv4tgn47kdqfwjcs5ohxjq3fn5egyqk6rmzhrsax3kxjkisn7ognjato42myx4v45fmda32rb3nfvqpmbidacx4s3wxj2h6czfa6bujfgyp4gfsbcarot7yopgp3rebe2jyepj7gs47eg3zto5b47brumljha32ld4ggiyjqusiiwnjvz3mpfjifyo24xi5odl5ussbaqook2qkpxhrelnoma5zljci5ycft5bto22fzuexljykg7tyxjnenbiw7hc5wnwuh65j3mqzhaquxedap4e5rzrqmxq',
-             '3a': 'fyxcr4d7a6qrfbklesclcxy7twio3lfxw7bw3eo2qwbnkvargkxa'
-        },
-        hashname: 'hkljlqir7dzkck2lkt5rb3lqijjbgehmf3tfacrmdgchxum2efsa'
-    }
-    callback(endpoint);
+TH.generateEndpoint = function(cb) {
+  Telehash.generate(function(err, endpoint) {
+    if (err) console.log("generate hashname error : " , err);
+    else cb(endpoint);
+  });
 };
